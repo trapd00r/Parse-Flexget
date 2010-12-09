7c84cb654669b42105c51d6acea106de7efa4d59
Magnus Woldrich <trapd00r@trapd00r.se>
2010-12-09 08:58:03 +0100

    Flexget.pm: added pod

--- lib/Parse/Flexget.pm
+++ lib/Parse/Flexget.pm
@@ -21,7 +21,53 @@ sub flexparse {
       push(@downloads, $1);
     }
   }
-
   return wantarray() ? @downloads : scalar(@downloads);
 }
 
+=pod
+
+=head1 NAME
+
+Parse::Flexget - Parse the flexget program output
+
+=head1 SYNOPSIS
+
+    use Parse::Flexget;
+
+    ...
+
+    print "$_\n" for flexparse(@logfile);
+
+=head1 DESCRIPTION
+
+Parse::Flexget parses the output from flexget and returns a list of downloaded files.
+
+=head1 EXPORTS
+
+=head2 flexparse()
+
+Parameters: @content, \@content
+
+Returns:    @downloads
+
+=head1 AUTHOR
+
+  Magnus Woldrich
+  CPAN ID: WOLDRICH
+  magnus@trapd00r.se
+  http://japh.se
+
+=head1 REPORTING BUGS
+
+Report bugs on rt.cpan.org or to magnus@trapd00r.se
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010 Magnus Woldrich. All right reserved.
+This program is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
+
+
+1;
